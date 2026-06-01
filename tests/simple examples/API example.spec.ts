1f88dc061f6585ea7f64e0e5e8370fd21b441529
YPorkhun <julposv@gmail.com>
2026-06-01 17:26:55 +0300

    test

--- tests/simple examples/API example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('GET status code 200 page', async ({ request }) => {
-  const response = await request.get('https://the-internet.herokuapp.com/status_codes/200');
-
-  expect(response.status()).toBe(200);
-});
